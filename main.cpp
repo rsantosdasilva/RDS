7b10e7e65058c3436ad71c8b75f062210c7f4265
rsantosdasilva <rsantosdasilva@btslivh.eu>
2022-01-11 16:05:27 +0100

    premier commit par , RSD

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+    cout << "bonjour" << endl; 
+    
+    return 0;
+}
+
